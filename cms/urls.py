5f912540e0ceb1ec4c580f708b788b896fac0733
farooq <farooqaziz1993@gmail.com>
2024-07-09 23:16:58 +0530

    added new app

---
+++ cms/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
+
+urlpatterns = [
+
+    path('dashboard', dashboard, name='dashboard'),
+
+  
+]
---
+++ users/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+USER_TYPE_CHOICES = (
+    ('adminuser', 'AdminUser'),
+    ('customer', 'Customer'),
+)
+
+class User(AbstractUser):
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=12, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    # Add related_name for groups
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+        related_query_name='user'
+    )
+
+    # Add related_name for user_permissions
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+        related_query_name='user'
+    )
