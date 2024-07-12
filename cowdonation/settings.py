c2ed3ccefc83d1ad4622067fe3af6b968a36c738
farooq <farooqaziz1993@gmail.com>
2024-07-12 19:45:47 +0530

    add middleware

--- cowdonation/settings.py
+++ cowdonation/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #"users.utils.loginmiddleware.UserTypeRedirectMiddleware" # Custome middleware for login
+    "users.utils.loginmiddleware.RestrictDonorMiddleware" # Custome middleware for login
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
